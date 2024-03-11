0886cc8eb0b95ee1fb259e79e92a81c15569025b
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-03-11 14:30:14 +0530

    created homw page

---
+++ src/Components/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function Home() {
+  return (
+    <div className=" h-screen flex justify-center items-center relative ">
+      <div className="py-7 shadow-lg px-9 bg-black  rounded-md text-white flex flex-col  items-center gap-3">
+        <h1 className="text-2xl mb-3">Blog-GPT</h1>
+        <p className="text-sm">
+          AI powered SAAS solution to genrate SEO optimizes blog post
+        </p>
+        <div className="w-full">
+          <button className="py-1 w-full bg-green-600 rounded-sm opacity-100 mt-3">
+            Begin
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
