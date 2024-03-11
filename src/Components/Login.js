f37bfd1d3e1ec5be9f73dd9d8ca6480481420403
gautamd23 <86918819+gautamd23@users.noreply.github.com>
2024-03-11 18:38:45 +0530

    fix validation error

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -10,9 +10,8 @@ export default function () {
     password: "",
   });
   const [toggleLogin, setToggleLogin] = useState(true);
-  const [loginError, setLoginError] = useState('')
-  const [loggedIn, setLoggedIn] = useState(false)
-  
+  const [loginError, setLoginError] = useState("");
+  const [error, setError] = useState({});
 
   function handleChange(e) {
     const { name, value } = e.target;
@@ -21,9 +20,24 @@ export default function () {
       [name]: value,
     });
   }
+
   async function handleSubmit(e) {
     e.preventDefault();
     console.log(formData);
+    let errors = {};
+    if (!formData.username.trim()) {
+      errors.username = "Username cannot be empty";
+    }
+    if (!formData.email.trim()) {
+      errors.email = "Email cannot be empty";
+    }
+    if (!formData.password.trim()) {
+      errors.password = "Password cannot be empty";
+    }
+    // if (Object.keys(errors).length !== 0) {
+    //   setError(errors);
+
+    // }
 
     try {
       const formDataJSON = JSON.stringify(formData);
@@ -51,40 +65,37 @@ export default function () {
             },
           }
         );
-        // console
-        
         console.log(response.data);
-        if(response.data ==="OK") {
-         setLoggedIn(true)
+        if (response.data === "OK") {
           navigate("/post");
           console.log("logged in..");
         } else {
-          
-          
+          setLoginError("Invalid login credentials");
         }
-        
       }
 
       resetForm();
     } catch (error) {
       console.log(error.response.data.error);
-      setLoginError(error.response.data.error)
+      setLoginError(error.response.data.error);
     }
   }
+
   function resetForm() {
     setFormData({
       username: "",
       email: "",
       password: "",
     });
+    setError({});
   }
-  
+
   return (
     <div className="flex justify-center items-center bg-black h-screen">
       <div className="">
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col gap-3  bg-white py-8 px-10 rounded-lg"
+          className="flex flex-col gap-3  bg-white py-8 px-10 "
         >
           <input
             className="outline-none  py-2 px-2  border-b-2"
@@ -94,15 +105,19 @@ export default function () {
             value={formData.username}
             onChange={handleChange}
           ></input>
+          {error.username && <p className="text-red-500">{error.username}</p>}
           {toggleLogin && (
-            <input
-              className="outline-none  py-2 px-2  border-b-2"
-              type="email"
-              placeholder="Eamil"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-            ></input>
+            <>
+              <input
+                className="outline-none  py-2 px-2  border-b-2"
+                type="email"
+                placeholder="Eamil"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+              ></input>
+              {error.email && <p className="text-red-500">{error.email}</p>}
+            </>
           )}
           <input
             className="outline-none py-2 px-2  border-b-2"
@@ -112,15 +127,23 @@ export default function () {
             value={formData.password}
             onChange={handleChange}
           ></input>
-          <p className="text-red-600 text-xs ">{loginError}</p>
+          {error.password && <p className="text-red-500">{error.password}</p>}
+          {Object.keys(error).length ? (
+            ""
+          ) : (
+            <>
+              <p className="text-red-600 text-xs ">{loginError}</p>
+            </>
+          )}
+
           <button
             type="submit"
-            className="py-1 px-6 bg-red-600 rounded-lg text-white mt-4"
+            className="py-1 px-6 bg-red-600  text-white mt-4"
           >
             {toggleLogin ? "Sign Up" : "Login"}
           </button>
           <p className="text-xs">
-            {toggleLogin ? "Already have an account?" : "New User?"}{" "}
+            {toggleLogin ? "Already have an account?" : "New User?"}
             <span
               className="font-bold text-xs cursor-pointer"
               onClick={() => setToggleLogin(!toggleLogin)}
